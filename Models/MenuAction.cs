942ce1049e5bdd02aefd865b2579debdb8d9e762
Matthéo Vinualez <mattheo.vinu@gmail.com>
2025-02-03 08:49:50 +0100

    Ajout d'un menu principal

---
+++ Models/MenuAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace easysave_project.Models {
+    internal class MenuAction {
+
+        public string name { get; set; }
+        public Action execute { get; set; }
+
+        public MenuAction(string name, Action execute) {
+            this.name = name;
+            this.execute = execute;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using easysave_project.View;
+using easysave_project.ViewModels;
+
+class Program {
+    static void Main(String[] args) {
+        var viewModel = new MainViewModel();
+        var menuView = new MenuView(viewModel);
+        menuView.Run();
+    }
+}
