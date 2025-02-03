b93fcb7b180cc43f2e9e8cc0ea210f459d8d0ccc
Matthéo Vinualez <mattheo.vinu@gmail.com>
2025-02-03 14:28:11 +0100

    Migration .NET 9 vers .NET 8 + Refactorisation MVC

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using easysave_project.View;
-using easysave_project.ViewModels;
+using easysave_project.Controller;
 
 class Program {
     static void Main(String[] args) {
-        var viewModel = new MainViewModel();
-        var menuView = new MenuView(viewModel);
+        var viewController = new MainViewController();
+        var menuView = new MenuView(viewController);
         menuView.Run();
     }
 }
