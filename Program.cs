592629430c83705c8ba66bf654c7102cd70bc91e
Manuel Dipp <manuel.dipp@viacesi.fr>
2025-02-04 15:50:17 +0100

    fix: changed the program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using easysave_project.View;
-using easysave_project.ViewModels;
 using easysave_project.Controllers;
 using easysave_project.Models;
 
 class Program {
     static void Main(String[] args) {
-        var viewModel = new MainViewModel();
+        var viewModel = new MainViewController();
         var menuView = new MenuView(viewModel);
         menuView.Run();
     }
